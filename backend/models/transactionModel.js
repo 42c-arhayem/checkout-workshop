85a5520ee9bae23b4261aa7a5d673f83bd6d96f9
Anthony <anthony.lonergan@42crunch.com>
2024-05-24 16:25:51 +0800

    Separate transfer and bill payments

--- backend/models/transactionModel.js
+++ backend/models/transactionModel.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 const transactionSchema = mongoose.Schema(
     {
         _id: false,
-        txnId: { type: String },
-        payer: { type: String, required: true },
-        payee: { type: String, required: true },
+        txnId: { type: String, required: true },
+        txnType: { type: String, required: true},
+        name: { type: String, required: true },
+        accountNumber: { type: String },
+        iban: { type: String},
         amount: { type: Number },
         currency: { type: String }
     },
